9fb2ca66e1c6b0764bdf1d327dddb756b80c379c
ryan mapa <ryan.m.mapa@gmail.com>
2017-08-29 17:18:25 -0700

    Add basic post and sub controller functionality

--- my_reddit/app/controllers/posts_controller.rb
+++ my_reddit/app/controllers/posts_controller.rb
@@ -1,22 +1,50 @@
 class PostsController < ApplicationController
+  before_action :require_login
+
   def new
+    render :new
   end
 
   def create
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    post.sub_id = params[:sub_id]
+    if post.save
+      redirect_to sub_url(post.sub)
+    else
+      flash[:errors]
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    post = current_user.posts.find(params[:id])
+    if post.update_attributes(post_params)
+      redirect_to sub_url(post.sub)
+    else
+      flash[:errors] = ["not yo stuff"]
+      render :edit
+    end
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to sub_url(post.sub)
   end
 
   def show
   end
 
   def index
+    #subs show to feature posts
   end
+
+  def post_params
+    params.require(:post).permit(:title, :url, :content)
+  end
+
 end
--- my_reddit/app/controllers/subs_controller.rb
+++ my_reddit/app/controllers/subs_controller.rb
@@ -1,17 +1,36 @@
 class SubsController < ApplicationController
+  before_action :require_login
+
   def new
+    render :new
   end
 
   def create
+    sub = Sub.new(sub_params)
+    sub.user_id = current_user.id
+    if sub.save
+      redirect_to sub_url(sub)
+    else
+      flash[:errors] = ["some stuff"]
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    sub = current_user.subs.find(params[:id])
+    if sub.update_attributes(sub_params)
+      redirect_to sub_url(sub)
+    else
+      flash[:errors] = ["boom"]
+      render :edit
+    end
   end
 
   def destroy
+    #ooptional
   end
 
   def show
@@ -19,4 +38,9 @@ class SubsController < ApplicationController
 
   def index
   end
+
+  def sub_params
+    params.require(:sub).permit(:title, :description)
+  end
+
 end
